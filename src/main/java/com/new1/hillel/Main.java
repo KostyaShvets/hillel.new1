c8a0273ede511febceae388a697d1aa0c0d83b4f
VerbovetskyiD <verbovetskyi.d@gmail.com>
2021-03-21 14:55:48 +0200

    Massive

--- src/main/java/com/new1/hillel/Main.java
+++ src/main/java/com/new1/hillel/Main.java
@@ -2,7 +2,18 @@ package main.java.com.new1.hillel;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from git");
-        System.out.println("new");
+        String [] array = new String[2];
+        int [] array2 = {1,2,3};
+
+        array[0] = "Hello";
+        array[1] = "world";
+        System.out.println(array[array.length-1]);
+
+   //     for (int i = 0; i<array.length; i++){
+   //         System.out.println(array[i]);
+    //    }
+   //     for (int i = 0; i<array2.length; i++){
+   //        System.out.println(array2[i]);
+    //    }
     }
 }
